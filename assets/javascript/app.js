2736f0cf70f05e61f3fd5810961127212b96e191
Nirav Patel <nrvpatel03@gmail.com>
2018-05-27 18:11:51 -0700

    some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
     //global variables
     //Scroll magic controller here
     //functions for populating content
-    
+    function populateIntro(){
+        var introTitle = $("<p><span class='red-text'>Hi</span>!</p>");
+        var introContent = $("<p>My name is <span class='red-text'>Nirav</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
+        $(".content").append(introTitle).append(introContent);
+    }
+    populateIntro();
 })
