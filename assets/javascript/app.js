e6210660549f4e1e8a5a2f0d4f2d92266a3f0ff1
Nirav Patel <nrvpatel03@gmail.com>
2018-05-29 12:09:36 -0700

    styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,19 +3,35 @@ $(document).ready(function(){
     //Scroll magic controller here
     //functions for materialize
     $(".dropdown-trigger").dropdown({
-        hover: true,
-        belowOrigin: true,
         alignment: 'left',
         coverTrigger: false,
         inDuration: 200,
-        outDuration: 400,
-        closeOnClick: false
+        outDuration: 400
     });
-    //functions for populating content
-    function populateIntro(){
-        var introTitle = $("<p class='head-text'><span class='red-text'>Hi</span>!</p>");
-        var introContent = $("<p id='intro-text'>My name is <span class='red-text'>Nirav</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
-        $(".content").append(introTitle).append(introContent);
-    }
-    populateIntro();
+    //populating content
+    var introTitle = $("<p class='currentTitle' id='intro-headText'><span class='red-text'>Hi</span>!</p>");
+    var introContent = $("<p class='currentContent' id='intro-text'>My name is <span class='red-text'>Nirav</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
+    var introDiv =$("<div class='content'></div>");
+
+    var aboutTitle = $("<p class='currentTitle' id='about-headText'><span class='red-text'>About Me</span>:</p>");
+    var aboutContent = $("<p id='about-text' class='currentContent'>SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText</p>")
+    var aboutDiv =$("<div class='content'></div>");
+
+    
+    introDiv.append(introTitle).append(introContent);
+    aboutDiv.append(aboutTitle).append(aboutContent);
+    $("#attachDiv").append(introDiv).append(aboutDiv);
+    introDiv.show();
+    aboutDiv.hide();
+//click functions
+    $("#aboutPage").click(function(){
+        introDiv.fadeOut(1000,function(){
+            aboutDiv.fadeIn(1000);
+        });
+    })
+    $("#homePage").click(function(){
+        aboutDiv.fadeOut(1000,function(){
+            introDiv.fadeIn(1000);
+        });
+    })
 })
