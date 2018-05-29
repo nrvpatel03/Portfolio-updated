0e0b88c23bed71f99917dacc2a24f098628b5f8f
Nirav Patel <nrvpatel03@gmail.com>
2018-05-29 13:04:21 -0700

    styling and navigating to other pages fade animation

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,4 @@
-$(document).ready(function(){
-    //global variables
-    //Scroll magic controller here
+$(document).ready(function () {
     //functions for materialize
     $(".dropdown-trigger").dropdown({
         alignment: 'left',
@@ -9,29 +7,81 @@ $(document).ready(function(){
         outDuration: 400
     });
     //populating content
-    var introTitle = $("<p class='currentTitle' id='intro-headText'><span class='red-text'>Hi</span>!</p>");
-    var introContent = $("<p class='currentContent' id='intro-text'>My name is <span class='red-text'>Nirav</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
-    var introDiv =$("<div class='content'></div>");
+    function populateIntro() {
+        var introTitle = $("<p id='intro-headText'><span class='red-text'>Hi</span>!</p>");
+        var introContent = $("<p id='intro-text'>My name is <span class='red-text'>Nirav</span>. <br>I'm a <span class='red-text'>full stack web developer</span>. <br>Welcome to my portfolio site!</p>");
+        var introDiv = $("<div class='content'></div>");
+        introDiv.attr("value", 1);
+        introDiv.append(introTitle).append(introContent);
+        return introDiv;
+    }
 
-    var aboutTitle = $("<p class='currentTitle' id='about-headText'><span class='red-text'>About Me</span>:</p>");
-    var aboutContent = $("<p id='about-text' class='currentContent'>SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText</p>")
-    var aboutDiv =$("<div class='content'></div>");
+    function populateAbout() {
+        var aboutTitle = $("<p id='about-headText'><span class='red-text'>About Me</span>:</p>");
+        var aboutContent = $("<p id='about-text'>SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText SampleText</p>")
+        var aboutDiv = $("<div class='content'></div>");
+        aboutDiv.attr("value", 2);
+        aboutDiv.append(aboutTitle).append(aboutContent);
+        return aboutDiv;
+    }
 
-    
-    introDiv.append(introTitle).append(introContent);
-    aboutDiv.append(aboutTitle).append(aboutContent);
-    $("#attachDiv").append(introDiv).append(aboutDiv);
-    introDiv.show();
-    aboutDiv.hide();
-//click functions
-    $("#aboutPage").click(function(){
-        introDiv.fadeOut(1000,function(){
-            aboutDiv.fadeIn(1000);
-        });
-    })
+    function populateContact() {
+        var contactTitle = $("<p id='contact-headText' class='red-text'>Contact</p>");
+        var contactDiv = $("<div class='content'></div>");
+        contactDiv.attr("value",4);
+        contactDiv.append(contactTitle);
+        return contactDiv;
+    }
+
+    function populatePortfolio() {
+        var portfolioTitle = $("<p id='portfolio-headText' class='red-text'>portfolio</p>");
+        var portfolioDiv = $("<div class='content'></div>");
+        portfolioDiv.attr("value",3);
+        portfolioDiv.append(portfolioTitle);
+        return portfolioDiv;
+    }
+
+    function changeDiv(cValue,valueNew,newDiv){
+        if(cValue != valueNew){
+            $("div[value=" + cValue + "]").fadeOut(1000,function(){
+                newDiv.fadeIn(500);
+            });
+        }
+    }
+    var currentValue = 0;
+    var intro = populateIntro();
+    var about = populateAbout();
+    var portfolio = populatePortfolio();
+    var contact = populateContact();
+    $("#attachDiv").append(intro).append(about).append(portfolio).append(contact);
+    //when user first loads page, only show intro
+    currentValue = 1;
+    about.hide();
+    portfolio.hide();
+    contact.hide();
+    console.log(typeof($("#homePage").attr("value")));
+    //click functions
     $("#homePage").click(function(){
-        aboutDiv.fadeOut(1000,function(){
-            introDiv.fadeIn(1000);
-        });
+        changeDiv(currentValue,1,intro);
+        currentValue = 1;
+        console.log(currentValue);
+    })
+    $("#topTitle").click(function(){
+        $("#homePage").trigger('click');
+    })
+    $("#aboutPage").click(function(){
+        changeDiv(currentValue,2,about);
+        currentValue = 2;
+        console.log(currentValue);
+    })
+    $("#portfolioPage").click(function(){
+        changeDiv(currentValue,3,portfolio);
+        currentValue = 3;
+        console.log(currentValue);
+    })
+    $("#contactPage").click(function(){
+        changeDiv(currentValue,4,contact);
+        currentValue = 4;
+        console.log(currentValue);
     })
 })
